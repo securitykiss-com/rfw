2d627d5a419b71076cb4162dad63f13e495dd5a2
greg-sk <grzegorz.luczywo@securitykiss.com>
2014-03-28 09:43:09 +0000

    tikz and convert to png trimming

--- docs/img/tex2png.sh
+++ docs/img/tex2png.sh
@@ -6,5 +6,11 @@ pdflatex $1.tex
 # Use ImageMagic's convert to get png. 
 # density determines the resolution and size.
 # flatten ensures white background
-convert -flatten -trim -density 150 -quality 100 $1.pdf $1.png
+convert -flatten -density 150 -quality 100 $1.pdf $1_page.png
+
+# trim margins
+convert $1_page.png -trim $1_trim.png
+
+convert $1_trim.png -bordercolor white -border 40 $1.png
+
 
